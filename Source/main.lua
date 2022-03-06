e6d8cd33d5510c6af01ba652a58b901012bad3d1
Fabricio C Zuardi <fabricio@fabricio.org>
2022-03-06 01:06:33 -0300

    ✨ feat(selection): select row or column with the dpad
    WIP

--- Source/main.lua
+++ Source/main.lua
@@ -1,13 +1,17 @@
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
 local gfx<const> = playdate.graphics
 
-local baseImagePath = "Images/kitten-drinking"
-
 -- main puzzle image, implemented using a tilemap
-local map = nil
+local puzzle = nil
 
-function makeMap(imageTable)
-    local totalTiles = imageTable:getLength()
+-- selected column and line indexes
+local selectedColumn, selectedLine = 1, 1
+
+-- UI sprites for selected column and selected line
+local uiSelectedColumn, uiSelectedLine = nil, nil
+
+function makeTileMap(imageTable)
     local totalColumns, totalLines = imageTable:getSize()
     local map = gfx.tilemap.new()
     map:setImageTable(imageTable)
@@ -52,16 +56,58 @@ function drawLines(tileMap)
 
 end
 
+function selectColumn(tileMap, uiColumnSprite, uiLineSprite, column)
+    local tileWidth, tileHeight = tileMap:getTileSize()
+    uiLineSprite:remove()
+    uiColumnSprite:moveTo(column * tileWidth - tileWidth / 2, 10)
+    uiColumnSprite:add()
+end
+
+function selectLine(tileMap, uiColumnSprite, uiLineSprite, line)
+    local tileWidth, tileHeight = tileMap:getTileSize()
+    uiColumnSprite:remove()
+    uiLineSprite:moveTo(10, line * tileHeight - tileHeight / 2)
+    uiLineSprite:add()
+end
+
+function drawBackground()
+    puzzle:draw(1, 1)
+    drawLines(puzzle)
+end
+
 function gameSetup()
-    local images, errorMessage = gfx.imagetable.new(baseImagePath)
-    assert(images, errorMessage)
-    map = makeMap(images)
-    shuffleMap(map)
-    map:draw(1, 1)
-    drawLines(map)
+    local downIcon, errorMessage = gfx.image.new("Images/down")
+    local rightIcon, errorMessage = gfx.image.new("Images/right")
+    local images, errorMessage = gfx.imagetable.new("Images/kitten-drinking")
+    puzzle = makeTileMap(images)
+    shuffleMap(puzzle)
+    gfx.setBackgroundColor(gfx.kColorClear)
+    uiSelectedColumn = gfx.sprite.new(downIcon)
+    uiSelectedLine = gfx.sprite.new(rightIcon)
+    drawBackground()
 end
 
 gameSetup()
 function playdate.update()
-
+    if playdate.buttonJustPressed(playdate.kButtonRight) then
+        selectedColumn = (selectedColumn < 4) and (selectedColumn + 1) or 1
+        selectColumn(puzzle, uiSelectedColumn, uiSelectedLine, selectedColumn)
+        drawBackground()
+    end
+    if playdate.buttonJustPressed(playdate.kButtonLeft) then
+        selectedColumn = (selectedColumn > 1) and (selectedColumn - 1) or 4
+        selectColumn(puzzle, uiSelectedColumn, uiSelectedLine, selectedColumn)
+        drawBackground()
+    end
+    if playdate.buttonJustPressed(playdate.kButtonDown) then
+        selectedLine = (selectedLine < 4) and (selectedLine + 1) or 1
+        selectLine(puzzle, uiSelectedColumn, uiSelectedLine, selectedLine)
+        drawBackground()
+    end
+    if playdate.buttonJustPressed(playdate.kButtonUp) then
+        selectedLine = (selectedLine > 1) and (selectedLine - 1) or 4
+        selectLine(puzzle, uiSelectedColumn, uiSelectedLine, selectedLine)
+        drawBackground()
+    end
+    gfx.sprite.update()
 end
